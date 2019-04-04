260141e9a0fd3d420985c873d93d087f6c1bd3d4
masdafaprimaaji <masdafaprimaaji@gmail.com>
2019-04-04 22:43:28 +0700

    4apr
    perubahan indexphp

--- application/views/home/index - Copy.php
+++
@@ -1,4 +0,0 @@
-<div class="container ">
-    <br><br><br><br>
-    <h1>hapus ae</h1>
-</div> 
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,7 +10,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
     <!-- MY CSS -->
-
     <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/style.css">
 
     <title><?php echo $judul ?></title>
@@ -29,7 +28,6 @@
                     <a class="nav-item nav-link" href="<?= base_url(); ?>">Home</a>
                     <a class="nav-item nav-link" href="<?= base_url(); ?>mahasiswa">Mahasiswa</a>
                     <a class="nav-item nav-link" href="#">About</a>
-
                 </div>
             </div>
         </div>
